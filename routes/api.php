112925de42b566d4ab66e7f04ea556bf78fb0bde
htereski <106633455+htereski@users.noreply.github.com>
2024-04-30 09:18:43 -0300

    add login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
