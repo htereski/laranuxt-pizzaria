5b1c94e8b728c9d5e25f8396527d0bddcd5fa92e
htereski <106633455+htereski@users.noreply.github.com>
2024-05-01 14:24:50 -0300

    corrected password field

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = Hash::make($request->passwrod);
+        $user->password = Hash::make($request->password);
         $user->role()->associate($role);
         $user->save();
 
